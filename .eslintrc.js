3a98a395aaa9327d47962f1f5a075aee8c7c0963
Ivan De Menezes <ivandmenezes@gmail.com>
2019-07-17 11:57:12 -0400

    :wrench: Add hooks lint rule
    Added rules to lint React Hooks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     node: true,
     es6: true
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -26,6 +26,8 @@ module.exports = {
     sourceType: 'module' // Allows for the use of imports
   },
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
     '@typescript-eslint/explicit-function-return-type': 'off'
   },
