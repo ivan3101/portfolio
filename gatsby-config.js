7b37afc4f0acceeb8533ff7737c7c6cb2cf59a70
Ivan De Menezes <ivandmenezes@gmail.com>
2019-07-20 19:58:10 -0400

    :sparkles: Add PurgeCSS support
    PurgeCSS remove any unused CSS from the production version, allowing to
    reduce the final size of the project.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-postcss`
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        tailwind: true
+      }
+    }
   ]
 }
