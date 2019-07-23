7140eb319335653df579489ee8b11e7b7ef8e5c9
Ivan De Menezes <ivandmenezes@gmail.com>
2019-07-22 22:35:01 -0400

    :sparkles: Create Hero component and Add it.

---
+++ src/components/hero.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Hero = () => (
+  <div>
+    <div>
+      <h1>Hi, I&apos;m Ivan De Menezes</h1>
+      <h2>
+        A Fullstack Web Developer and Linux enthisiast ready to Work with You
+      </h2>
+    </div>
+  </div>
+)
+
+export default Hero
