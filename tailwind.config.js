7c6dbeac2a5baa4c331cc8abfc32ed99f1f709ad
Ivan De Menezes <ivandmenezes@gmail.com>
2019-07-30 00:10:13 -0400

    :wrench: Add color palette to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   theme: {
+    colors: {
+      blue: "#1252A5",
+      yellow: "#FFA007",
+      gray: {
+        light: "#FAFAFA",
+        dark: "#424242"
+      },
+      white: "#FFFFFF"
+    },
     extend: {}
   },
   variants: {},
