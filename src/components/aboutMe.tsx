6652604ac8ef2b8fe711cc4f93a677268ed9b24a
Ivan De Menezes <ivandmenezes@gmail.com>
2019-07-30 23:13:09 -0400

    :lipstick: Rewrite AboutMe component to stick to new style guide.

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,16 +1,29 @@
 import React from "react"
-import profilePic from "../../images/profile.jpg"
+import Container from "./container"
+import profileImage from "../../images/profileImage.jpg"
 
 const AboutMe = () => (
-  <div>
-    <p>
-      I&apos;m an European Web Developer based in Venezuela. I have a degree in
-      Computer Engineering from the Jose Antonio Paez University. I love Linux
-      and I&apos;ve been using it since I was 12 years old. I&apos;ve been
-      programming for almost 6 years now, specialized in JavaScript and NodeJS.
-    </p>
-    <img src={profilePic} className="" />
-  </div>
+  <section className="pt-12 pb-12 bg-white">
+    <Container>
+      <h2 className="font-semibold text-2xl text-center">ABOUT</h2>
+      <article className="flex mt-12 items-center">
+        <p className="w-1/2 px-16 leading-loose">
+          I&apos;m an European Web Developer based in Venezuela. I have a degree
+          in Computer Engineering from the Jose Antonio Paez University. I love
+          Linux and I&apos;ve been using it since I was 12 years old. I&apos;ve
+          been programming for almost 6 years now, specialized in JavaScript and
+          NodeJS.
+        </p>
+        <div className="w-1/2">
+          <img
+            src={profileImage}
+            alt="Ivan's photo"
+            className="h-64 w-64 rounded-full mx-auto"
+          />
+        </div>
+      </article>
+    </Container>
+  </section>
 )
 
 export default AboutMe
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Hero from "../components/hero"
 import Navbar from "../components/navbar"
+import AboutMe from "../components/aboutMe"
 
 const Index = () => (
   <React.Fragment>
     <main>
       <Hero />
       <Navbar />
+      <AboutMe />
     </main>
   </React.Fragment>
 )
