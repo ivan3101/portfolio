0b947cb3f1e9524e84645a7a536cac52ef363e5d
Ivan De Menezes <ivandmenezes@gmail.com>
2019-08-04 00:02:43 -0400

    :recycle: Refactor flex container of icons in new component.

---
+++ src/components/iconsList.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const IconsList: React.FC = ({ children }) => (
+  <article className="flex flex-row flex-wrap justify-center">
+    {children}
+  </article>
+)
+
+export default IconsList
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,21 +2,22 @@ import React from "react"
 import Container from "./container"
 import SectionHeader from "./sectionHeader"
 import SkillIconContainer from "./skillIconContainer"
+import IconsList from "./iconsList"
 
 const Skills = () => (
   <section className="py-12 bg-gray">
     <Container>
       <SectionHeader>SKILLS</SectionHeader>
-      <p className="mx-auto text-center px-48 mt-6 mb-10">
+      <p className="mx-auto text-center px-48 mt-6 mb-5">
         In my time as a Web Developer, I learned a variety of Computational
         Languages, Libraries, Frameworks, Databases and Tools. I’m specialized
         in JavaScript and use it for both, Front-end and Back-end development.
         In Front-end I find myself mostly using ReactJS, and ExpressJS for the
         Back-end.
       </p>
-      <article className="flex flex-row flex-wrap justify-center">
+      <IconsList>
         <SkillIconContainer />
-      </article>
+      </IconsList>
     </Container>
   </section>
 )
