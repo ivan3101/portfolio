a94e153a6cd096b74b22999ea0b76be2990f699b
Ivan De Menezes <ivandmenezes@gmail.com>
2019-08-03 23:55:01 -0400

    :recycle: Refactor to use inline SVG.

--- src/components/skillIcon.tsx
+++ src/components/skillIcon.tsx
@@ -1,9 +1,8 @@
 import React from "react"
+import SVG, { IProps } from "react-inlinesvg"
 
-const SkillIcon: React.FC<
-  React.PropsWithoutRef<JSX.IntrinsicElements["img"]>
-> = ({ alt, src }) => (
-  <img src={src} alt={alt} className="w-12 h-12 mx-4 mb-5" />
+const SkillIcon: React.FC<IProps> = ({ src }) => (
+  <SVG src={src} className="w-12 h-12 mx-4 mt-5" />
 )
 
 export default SkillIcon
