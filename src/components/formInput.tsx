7bd24ba8720ec9b4674454794ed9f81bb6e1413b
Ivan De Menezes <ivandmenezes@gmail.com>
2019-08-20 19:16:28 -0400

    Change input font size.

--- src/components/formInput.tsx
+++ src/components/formInput.tsx
@@ -9,7 +9,7 @@ const FormInput: React.FC<
   React.PropsWithoutRef<JSX.IntrinsicElements["input"]> & FormInputProps
 > = ({ error, type = "text", ...props }) => {
   let classes =
-    "text-grey-dark block w-full px-2 py-1 mb-1 rounded border-solid border-2 bg-white focus:bg-white "
+    "text-base text-grey-dark block w-full px-2 py-1 mb-1 rounded border-solid border-2 bg-white focus:bg-white "
 
   classes += error
     ? `border-red focus:border-red ${formInputStyles.error}`
