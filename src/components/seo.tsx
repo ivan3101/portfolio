3484ce121600c12067550ae1965f16e219891f5a
Ivan De Menezes <ivandmenezes@gmail.com>
2019-08-26 21:01:19 -0400

    :bug: Fix twitter metadata tags and favicon.

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -28,104 +28,56 @@ const SEO = () => {
       </title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
+      <meta name="image" content={siteUrl + "/" + image} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={siteUrl} />
-      <meta property="og:image" content={image} />
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={siteUrl} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={image} />
+      <meta property="og:image" content={siteUrl + "/" + image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:url" content={siteUrl} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:creator" content="@ivandmnzs" />
+      <meta name="twitter:image" content={siteUrl + "/" + image} />
       <link rel="canonical" href={siteUrl} />
 
-      {/* eslint-disable react/no-string-refs */}
-      <link ref="icon" href="favicon.ico" />
+      <link rel="icon" href={siteUrl + "/favicon.ico"} />
       <link
-        rel="apple-touch-icon-precomposed"
-        sizes="57x57"
-        href="apple-touch-icon-57x57.png"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        sizes="114x114"
-        href="apple-touch-icon-114x114.png"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        sizes="72x72"
-        href="apple-touch-icon-72x72.png"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        sizes="144x144"
-        href="apple-touch-icon-144x144.png"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        sizes="60x60"
-        href="apple-touch-icon-60x60.png"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        sizes="120x120"
-        href="apple-touch-icon-120x120.png"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        sizes="76x76"
-        href="apple-touch-icon-76x76.png"
-      />
-      <link
-        rel="apple-touch-icon-precomposed"
-        sizes="152x152"
-        href="apple-touch-icon-152x152.png"
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={siteUrl + "/apple-touch-icon.png"}
       />
       <link
         rel="icon"
         type="image/png"
-        href="favicon-196x196.png"
-        sizes="196x196"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        href="favicon-96x96.png"
-        sizes="96x96"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        href="favicon-32x32.png"
         sizes="32x32"
+        href={siteUrl + "/favicon-32x32.png"}
       />
       <link
         rel="icon"
         type="image/png"
-        href="favicon-16x16.png"
+        sizes="192x192"
+        href={siteUrl + "/android-chrome-192x192.png"}
+      />
+      <link
+        rel="icon"
+        type="image/png"
         sizes="16x16"
+        href={siteUrl + "/favicon-16x16.png"}
       />
+      <link rel="manifest" href={siteUrl + "/site.webmanifest"} />
       <link
-        rel="icon"
-        type="image/png"
-        href="favicon-128.png"
-        sizes="128x128"
+        rel="mask-icon"
+        href={siteUrl + "/safari-pinned-tab.svg"}
+        color="#5bbad5"
       />
-      <meta name="application-name" content="&nbsp;" />
-      <meta name="msapplication-TileColor" content="#FFFFFF" />
-      <meta name="msapplication-TileImage" content="mstile-144x144.png" />
-      <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+      <meta name="msapplication-TileColor" content="#00aba9" />
       <meta
-        name="msapplication-square150x150logo"
-        content="mstile-150x150.png"
+        name="msapplication-TileImage"
+        content={siteUrl + "/mstile-144x144.png"}
       />
-      <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
-      <meta
-        name="msapplication-square310x310logo"
-        content="mstile-310x310.png"
-      />
-      {/* eslint-enable react/no-string-refs */}
+      <meta name="theme-color" content="#ffffff" />
     </Helmet>
   )
 }
